e1be13ceef629f893e1d30d3339fd9496ca054f6
Matcha-Inori <513034940@qq.com>
2016-08-17 06:57:21 +0800

    使用pipedoutputstream可能会造成阻塞,所以这个地方不能使用这个流,先写出到byte[]中,然后再读取

--- TestExternalizable/src/com/matcha/app/TestExternalizable.java
+++ TestExternalizable/src/com/matcha/app/TestExternalizable.java
@@ -1,8 +1,7 @@
 package com.matcha.app;
 
-import com.matcha.data.ExternalizableObj;
-import com.matcha.data.NormalSerializable;
-import com.matcha.data.ReplaceAndResolveClass;
+import com.matcha.data.*;
+import com.sun.xml.internal.xsom.impl.WildcardImpl;
 
 import java.io.*;
 
@@ -13,23 +12,52 @@ public class TestExternalizable
 {
     public static void main(String[] args)
     {
+        byte[] bytes = writeSerializable();
+        readSerializable(bytes);
+    }
+
+    public static byte[] writeSerializable()
+    {
         try(
-                PipedInputStream pipedInputStream = new PipedInputStream();
-                PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream);
-                ObjectOutput objectOutput = new ObjectOutputStream(pipedOutputStream);
-                ObjectInput objectInput = new ObjectInputStream(pipedInputStream)
-        )
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)
+                )
         {
             ReplaceAndResolveClass replaceAndResolveClass = new ReplaceAndResolveClass("为美好的世界献上祝福");
             ExternalizableObj externalizableObj = new ExternalizableObj("Matcha", 23, replaceAndResolveClass,
                     "Bing", "Bang", "Ha");
-            NormalSerializable normalSerializable = new NormalSerializable("Randone", "你看不到这段话的", 100,
-                    externalizableObj, "ABC", "DEF");
-            System.out.println("NormalSerializable is --- " + normalSerializable);
-            objectOutput.writeObject(normalSerializable);
-            objectOutput.flush();
-            NormalSerializable getNormalSerializable = (NormalSerializable) objectInput.readObject();
-            System.out.println("NormalSerializable is --- " + getNormalSerializable);
+            OtherNormalSerializable otherNormalSerializable =
+                    new OtherNormalSerializable("这个不会序列化", "own1", "own2", "own3", "own3");
+            SubNormalSerializable subNormalSerializable = new SubNormalSerializable("Randone", "你看不到这段话的", 100,
+                    externalizableObj, otherNormalSerializable, "ABC", "DEF");
+            subNormalSerializable.add("TEST---1");
+            subNormalSerializable.add("TEST---2");
+            subNormalSerializable.add("TEST---3");
+            subNormalSerializable.add("TEST---4");
+            System.out.println(subNormalSerializable.pop());
+            System.out.println(subNormalSerializable.pop());
+            System.out.println(subNormalSerializable.pop());
+            System.out.println("SubNormalSerializable is --- " + subNormalSerializable);
+            objectOutputStream.writeObject(subNormalSerializable);
+            return byteArrayOutputStream.toByteArray();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Object readSerializable(byte[] bytes)
+    {
+        try(
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+                ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)
+                )
+        {
+            SubNormalSerializable getSubNormalSerializable = (SubNormalSerializable) objectInputStream.readObject();
+            System.out.println("SubNormalSerializable is --- " + getSubNormalSerializable);
+            return getSubNormalSerializable;
         }
         catch (IOException | ClassNotFoundException e)
         {
