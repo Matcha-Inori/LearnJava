f33f901edbab65d7f80900b42510a35956966ed8
xidong_ren <513034940@qq.com>
2016-09-29 17:49:21 +0800

    增加了测试BufferedIO的测试类

---
+++ Test/src/com/matcha/io/TestBufferedIO.java
@@ -0,0 +1,94 @@
+package com.matcha.io;
+
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+public class TestBufferedIO
+{
+    public static void main(String[] args)
+    {
+        String filePath = "resource" + File.separator + "txt" + File.separator + "bigFile.txt";
+
+        preparedFile(filePath);
+
+        try (
+                FileInputStream fileInputStream = new FileInputStream(filePath);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 1024)
+        )
+        {
+            byte[] readBytes = new byte[500];
+            int allCount = 0;
+            System.out.println("buffered read start!");
+            long stratTime = System.nanoTime();
+            for(int size = 0;(size = bufferedInputStream.read(readBytes)) > 0;)
+            {
+                allCount += size;
+//                System.out.println("read - " + size + " and all count is " + allCount);
+            }
+            long endTime = System.nanoTime();
+            System.out.println("buffered read finish! cost - " + (endTime - stratTime) + " and allCount - " + allCount);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("");
+
+        try(FileInputStream fileInputStream = new FileInputStream(filePath))
+        {
+            byte[] readBytes = new byte[500];
+            int allCount = 0;
+            System.out.println("normal read start!");
+            long stratTime = System.nanoTime();
+            for(int size = 0;(size = fileInputStream.read(readBytes)) > 0;)
+            {
+                allCount += size;
+//                System.out.println("read - " + size + " and all count is " + allCount);
+            }
+            long endTime = System.nanoTime();
+            System.out.println("normal read finish! cost - " + (endTime - stratTime) + " and allCount - " + allCount);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void preparedFile(String filePath)
+    {
+        File file = new File(filePath);
+        if (file.exists() && file.isFile())
+            file.delete();
+
+        try(
+                RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
+                FileChannel fileChannel = randomAccessFile.getChannel()
+        )
+        {
+            MappedByteBuffer mappedByteBuffer =
+                    fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 500 * 1024 * 1024);
+            for(int remaining = 0;(remaining = mappedByteBuffer.remaining()) > 0;)
+            {
+                int size = Math.min(1024, remaining);
+                byte[] writeBytes = new byte[size];
+                for(int index = 0;index < size;index++)
+                {
+                    writeBytes[index] = (byte) index;
+                }
+                mappedByteBuffer.put(writeBytes);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
--- Test/src/com/matcha/io/TestCipherIO.java
+++ Test/src/com/matcha/io/TestCipherIO.java
@@ -1,8 +1,119 @@
 package com.matcha.io;
 
+import javax.crypto.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.security.*;
+
 /**
  * Created by Matcha on 16/9/26.
  */
 public class TestCipherIO
 {
+    public static void main(String[] args)
+    {
+        Cipher rsaCipherEncrypt = null;
+        Cipher rsaCipherDecrypt = null;
+        Cipher aesCipherEncrypt = null;
+        SecretKey secretKey = null;
+        try
+        {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            KeyPair keypair = keyPairGenerator.generateKeyPair();
+            PublicKey publicKey = keypair.getPublic();
+            PrivateKey privateKey = keypair.getPrivate();
+
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            secretKey = keyGenerator.generateKey();
+
+            rsaCipherEncrypt = Cipher.getInstance("RSA");
+            rsaCipherEncrypt.init(Cipher.ENCRYPT_MODE, privateKey);
+
+            aesCipherEncrypt = Cipher.getInstance("AES");
+            aesCipherEncrypt.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            rsaCipherDecrypt = Cipher.getInstance("RSA");
+            rsaCipherDecrypt.init(Cipher.DECRYPT_MODE, publicKey);
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        byte[] bytesPart1 = null;
+        try(
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                CipherOutputStream cipherOutputStream = new CipherOutputStream(byteArrayOutputStream, rsaCipherEncrypt);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(cipherOutputStream)
+        )
+        {
+            objectOutputStream.writeObject(secretKey);
+            bytesPart1 = byteArrayOutputStream.toByteArray();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        byte[] bytesPart2 = null;
+        try(
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                CipherOutputStream cipherOutputStream = new CipherOutputStream(byteArrayOutputStream, aesCipherEncrypt)
+        )
+        {
+            cipherOutputStream.write("今天也是个好天气!!!".getBytes(Charset.forName("UTF-8")));
+            bytesPart2 = byteArrayOutputStream.toByteArray();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        SecretKey secretKeyFromRead = null;
+        try(
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesPart1);
+                CipherInputStream cipherInputStream = new CipherInputStream(byteArrayInputStream, rsaCipherDecrypt);
+                ObjectInputStream objectInputStream = new ObjectInputStream(cipherInputStream)
+        )
+        {
+            secretKeyFromRead = (SecretKey) objectInputStream.readObject();
+        }
+        catch (IOException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        Cipher aesCipherDecrypt = null;
+        try
+        {
+            aesCipherDecrypt = Cipher.getInstance("AES");
+            aesCipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeyFromRead);
+        }
+        catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        try(
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytesPart2);
+                CipherInputStream cipherInputStream = new CipherInputStream(byteArrayInputStream, aesCipherDecrypt)
+        )
+        {
+            int available = cipherInputStream.available();
+            byte[] strBytes = new byte[available];
+            cipherInputStream.read(strBytes);
+            String str = new String(strBytes, Charset.forName("UTF-8"));
+            System.out.println(str);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
